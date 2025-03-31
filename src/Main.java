cc8e3ce3e2308751bc0e45277414226da6d78f48
Langerais <dorfinmax@gmail.com>
2025-03-31 13:00:21 +0300

    basic StringReverce done;

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Arrays;
+
 public class Main {
+
+    //Given a string of words delimited by spaces, reverse the words in string. For example, given "hello world here", return "here world hello"
+    //
+    //Follow-up: given a mutable string representation, can you perform this operation in-place?
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        stringReverse("hello world here");
+
+    }
+
+    public static String stringReverse(String s){
+
+
+        String[] temp = s.split(" ");
+        String[] out = new String[temp.length];
+
+        for(int i = 0; i < out.length; i++){
+            out[i] = temp[temp.length - i - 1];
+        }
+
+        return Arrays.toString(out);
     }
 }
