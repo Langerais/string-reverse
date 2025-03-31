64bfbe81662931598c3417836426062833c8bbf0
Langerais <dorfinmax@gmail.com>
2025-03-31 13:07:10 +0300

    Added input to the basic Reverse; Implemented the reverce in place;

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,50 @@
-import java.util.Arrays;
-
 public class Main {
 
-    //Given a string of words delimited by spaces, reverse the words in string. For example, given "hello world here", return "here world hello"
-    //
-    //Follow-up: given a mutable string representation, can you perform this operation in-place?
-
     public static void main(String[] args) {
+        String input = "hello world here";
 
-        stringReverse("hello world here");
+        System.out.println("Basic Reverse:");
+        String reversed = stringReverse(input);
+        System.out.println(reversed);
 
+        char[] inputChars = input.toCharArray();
+        reverseWordsInPlace(inputChars);
+        System.out.println("In Place Reverse:");
+        System.out.println(new String(inputChars));
     }
 
-    public static String stringReverse(String s){
-
-
-        String[] temp = s.split(" ");
-        String[] out = new String[temp.length];
-
-        for(int i = 0; i < out.length; i++){
-            out[i] = temp[temp.length - i - 1];
+    public static String stringReverse(String s) {
+        String[] words = s.trim().split("\\s+");
+        int left = 0, right = words.length - 1;
+        while (left < right) {
+            String temp = words[left];
+            words[left] = words[right];
+            words[right] = temp;
+            left++;
+            right--;
         }
+        return String.join(" ", words);
+    }
 
-        return String.join(" ", out);
+    public static void reverseWordsInPlace(char[] s) {
+        // Reverse the whole array
+        reverse(s, 0, s.length - 1);
+
+        // Reverse each word
+        int start = 0;
+        for (int end = 0; end <= s.length; end++) {
+            if (end == s.length || s[end] == ' ') {
+                reverse(s, start, end - 1);
+                start = end + 1;
+            }
+        }
+    }
+
+    private static void reverse(char[] arr, int left, int right) {
+        while (left < right) {
+            char temp = arr[left];
+            arr[left++] = arr[right];
+            arr[right--] = temp;
+        }
     }
 }
